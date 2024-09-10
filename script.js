9819d4f6944b728240b11d60f0c234e8e56e5417
Ludvig Alvir <ludvi@OSL_Ludvig_FED1.res.bitpro.as>
2024-09-10 23:17:05 +0200

    first working commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const nameCells = document.querySelectorAll(".c2");
+
+const names = Array.from(nameCells).map((item, i) => {
+  if (i) {
+    return item.childNodes[0].innerText;
+  }
+});
+const submissionCells = document.querySelectorAll(".c13");
+
+const submissions = Array.from(submissionCells).map((item, i) => {
+  if (i) {
+    return item.lastChild.innerText;
+  }
+});
+const data = names.map((item, i) => {
+  return { name: item, submission: submissions[i] };
+});
+
+function downloadJSON(jsonData, filename = "data.json") {
+  const jsonString = JSON.stringify(jsonData, null, 2); // Pretty print JSON with indentation
+  const blob = new Blob([jsonString], { type: "application/json" });
+  const url = window.URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.setAttribute("href", url);
+  a.setAttribute("download", filename);
+  a.click();
+
+  window.URL.revokeObjectURL(url);
+}
+
+// Call the download function with the data
+downloadJSON(data);
